f2b5dd442fbd7fb4aa712fd789b0d9634c7f6619
Erin Epperson <eke94593@creighton.edu>
2021-02-24 15:55:06 -0600

    Create Form1.js

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,5 @@
+let petNames = ["Darcy","Jack","Jerry","Marty","Vito","Vinny","Riley","Saddie","Marge","Sam"]
+let withSal = petNames.push("Sal")
+console.log(`The third pet is named ${petNames[2]}.`)
+console.log(`The array has ${petNames.length} in it.`)
+petNames.pop()
